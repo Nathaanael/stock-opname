f827edd994185133c92d88ccfe36c36db158bb42
Nathanael <22k10006@student.unika.ac.id>
2026-06-16 18:27:26 +0700

    fix: paksa arahkan cache dan storage laravel ke tmp

--- api/index.php
+++ api/index.php
@@ -1,4 +1,22 @@
 <?php
 // File ini dibuat khusus untuk Vercel agar bisa menjalankan Laravel (Serverless PHP)
+
+// Paksa Laravel untuk meletakkan semua file cache di folder /tmp yang diizinkan oleh Vercel
+$paths = [
+    'APP_SERVICES_CACHE' => '/tmp/services.php',
+    'APP_PACKAGES_CACHE' => '/tmp/packages.php',
+    'APP_CONFIG_CACHE' => '/tmp/config.php',
+    'APP_ROUTES_CACHE' => '/tmp/routes.php',
+    'APP_EVENTS_CACHE' => '/tmp/events.php',
+    'VIEW_COMPILED_PATH' => '/tmp',
+    'LARAVEL_STORAGE_PATH' => '/tmp',
+];
+
+foreach ($paths as $key => $value) {
+    putenv("$key=$value");
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
 // Akan mengarahkan request ke public/index.php
 require __DIR__ . '/../public/index.php';
