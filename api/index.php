e57d9de793b03b79d0197a259524ec1ea39b6570
Nathanael <22k10006@student.unika.ac.id>
2026-06-16 18:19:56 +0700

    fix: up to vercel

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+// File ini dibuat khusus untuk Vercel agar bisa menjalankan Laravel (Serverless PHP)
+// Akan mengarahkan request ke public/index.php
+require __DIR__ . '/../public/index.php';
